eb0c541e70714182d70ae31acbf3565acf8160ad
KarinaV-cyber <virtualkarina2553@gmail.com>
2025-10-25 13:46:59 -0400

    Added JS function and moment/timezone cdn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-//alert("hello!");
+//Los Angeles
+function updateTime(){
+let losAngelesElement=document.querySelector("#los-angeles");
+let losAngelesDateElement=losAngelesElement.querySelector(".date");
+let losAngelesTimeElement=losAngelesElement.querySelector(".time");
+let losAngelesTime=moment().tz("America/Los_Angeles");
+
+losAngelesDateElement.innerHTML= losAngelesTime.format("MMMM Do YYYY");
+losAngelesTimeElement.innerHTML=losAngelesTime.format("h:mm:ss:SSS[<small>]A[</small>]");
+ 
+//Sydney
+
+let sydneyElement=document.querySelector("#sydney");
+let sydneyDateElement=sydneyElement.querySelector(".date");
+let sydneyTimeElement=sydneyElement.querySelector(".time");
+let sydneyTime=moment().tz("Australia/Sydney");
+
+sydneyDateElement.innerHTML= sydneyTime.format("MMMM Do YYYY");
+sydneyTimeElement.innerHTML=sydneyTime.format("h:mm:ss:SSS[<small>]A[</small>]");
+
+//Lima
+let limaElement=document.querySelector("#lima");
+let limaDateElement=limaElement.querySelector(".date");
+let limaTimeElement=limaElement.querySelector(".time");
+let limaTime=moment().tz("America/Lima");
+
+limaDateElement.innerHTML= limaTime.format("MMMM Do YYYY");
+limaTimeElement.innerHTML=limaTime.format("h:mm:ss:SSS[<small>]A[</small>]");}
+
+updateTime();
+setInterval(updateTime, 1);
