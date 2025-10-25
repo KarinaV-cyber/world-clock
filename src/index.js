ce0085f7455ddc1d270a3e14d03b20c4c766a67d
KarinaV-cyber <virtualkarina2553@gmail.com>
2025-10-25 15:36:49 -0400

    Added the functionality for the select

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ let losAngelesTimeElement=losAngelesElement.querySelector(".time");
 let losAngelesTime=moment().tz("America/Los_Angeles");
 
 losAngelesDateElement.innerHTML= losAngelesTime.format("MMMM Do YYYY");
-losAngelesTimeElement.innerHTML=losAngelesTime.format("h:mm:ss:SSS[<small>]A[</small>]");
+losAngelesTimeElement.innerHTML=losAngelesTime.format("h:mm:ss:SS[<small>]A[</small>]");
  
 //Sydney
 
@@ -16,7 +16,7 @@ let sydneyTimeElement=sydneyElement.querySelector(".time");
 let sydneyTime=moment().tz("Australia/Sydney");
 
 sydneyDateElement.innerHTML= sydneyTime.format("MMMM Do YYYY");
-sydneyTimeElement.innerHTML=sydneyTime.format("h:mm:ss:SSS[<small>]A[</small>]");
+sydneyTimeElement.innerHTML=sydneyTime.format("h:mm:ss:SS[<small>]A[</small>]");
 
 //Lima
 let limaElement=document.querySelector("#lima");
@@ -25,7 +25,24 @@ let limaTimeElement=limaElement.querySelector(".time");
 let limaTime=moment().tz("America/Lima");
 
 limaDateElement.innerHTML= limaTime.format("MMMM Do YYYY");
-limaTimeElement.innerHTML=limaTime.format("h:mm:ss:SSS[<small>]A[</small>]");}
+limaTimeElement.innerHTML=limaTime.format("h:mm:ss:SS[<small>]A[</small>]");}
+
 
 updateTime();
-setInterval(updateTime, 1);
+setInterval(updateTime, 1000);
+function updateCity(event){
+    let cityTimeZone=event.target.value;
+    let cityName=cityTimeZone.replace("_", " ").split("/")[1];
+    let cityTime=moment().tz(cityTimeZone);
+    //console.log(cityTime.format("MMMM Do YYYY"));
+    let citiesElement=document.querySelector("#cities");
+    citiesElement.innerHTML= `<div class="city">
+    <div>
+                        <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+            </div>
+            <div class="time">${cityTime.format("h:mm:ss:SS")}<small>${cityTime.format("A")}</small></div>
+        </div>`;
+}
+let citiesSelectElement=document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
