5dc25d66e64543b1a71a95b6a47cda594ee66b28
KarinaV-cyber <virtualkarina2553@gmail.com>
2025-10-27 12:25:25 -0400

    Added the current location

--- src/index.js
+++ src/index.js
@@ -32,6 +32,9 @@ updateTime();
 setInterval(updateTime, 1000);
 function updateCity(event){
     let cityTimeZone=event.target.value;
+    if (cityTimeZone ==="current"){
+      cityTimeZone  = moment.tz.guess();
+    }
     let cityName=cityTimeZone.replace("_", " ").split("/")[1];
     let cityTime=moment().tz(cityTimeZone);
     //console.log(cityTime.format("MMMM Do YYYY"));
